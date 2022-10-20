3d2194a190cffcf4cff4de9378bf918c14419eff
Shanu Vashistha <shanumanishsaraswat@gmail.com>
2022-10-20 15:19:53 +0530

    Added cors json

--- tools/copy-webviewer-files.js
+++ tools/copy-webviewer-files.js
@@ -3,7 +3,6 @@ const fs = require('fs-extra');
 const copyFiles = async () => {
   try {
     await fs.copy('./node_modules/@pdftron/webviewer/public', './public/webviewer');
-    console.log('WebViewer files copied over successfully');
   } catch (err) {
     console.error(err);
   }
